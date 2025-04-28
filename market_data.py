014f92e98fc2b8a51a03087f076f140ed8776609
zhaoxi0418 <zhaoxi0418@outlook.com>
2025-04-28 15:59:40 +0800

    add ploting

--- market_data.py
+++ market_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import time
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
 
 def read_xls_files(files) -> list[pd.DataFrame]:
     df_list = []
@@ -12,7 +13,8 @@ def pop_row(df: pd.DataFrame) -> (pd.DataFrame, pd.Series):
     df = df.iloc[1:]
     return df, row
 
-def execute(files_and_target_ratios: dict, start_date: str, end_date: str, cycle: int):
+
+def execute(files_and_target_ratios: dict, start_date: str, end_date: str, cycle: int, debug=False):
     '''
     :param files: xls文件列表和持仓比例（dict）
     :param target_ratio: 目标持仓比例
@@ -66,7 +68,7 @@ def execute(files_and_target_ratios: dict, start_date: str, end_date: str, cycle
         start_timestamp += cycle * 24 * 60 * 60
 
     while cur_day < end_date:
-        print(cur_day)
+
         today_money = []
         for i in range(len(date_and_price_list)):
             df = date_and_price_list[i]
@@ -97,7 +99,9 @@ def execute(files_and_target_ratios: dict, start_date: str, end_date: str, cycle
             data_this_day.append(m)
         data_this_day.append(sum(today_money))
         data_each_day.append(data_this_day)
-        print(data_this_day)
+        if debug:
+            print(cur_day)
+            print(data_this_day)
 
         cur_timestamp = pd.Timestamp(cur_day).timestamp()
         cur_timestamp += 24 * 60 * 60
@@ -106,24 +110,109 @@ def execute(files_and_target_ratios: dict, start_date: str, end_date: str, cycle
     return data_each_day[1:] # 去掉dummy头部
 
 
+def cal_yearly_rate(date_money: list[(str, float)]) -> float:
+    '''
+    计算年化收益率
+    :param date_money:
+    :return: 年化收益率
+    '''
+    yearly_rate = ((date_money[-1][1] / date_money[0][1]) ** (365 / len(date_money))) - 1
+    return yearly_rate
+
+
+def cal_max_recall(date_money: list[(str, float)]) -> float:
+    '''
+    计算最大回撤
+    :param date_money:
+    :return: 最大回撤
+    '''
+    cur_max = []
+    cur_recall = []
+    for item in date_money:
+        if len(cur_max) == 0 or item[1] > cur_max[-1][1]:
+            cur_max.append(item)
+        else:
+            cur_max.append(cur_max[-1])
+        cur_recall.append(1 - item[1] / cur_max[-1][1])
+    max_recall = max(cur_recall)
+    idx = cur_recall.index(max_recall)
+    print(f"最大回撤发生于{date_money[idx][0]}，当前最大值{cur_max[idx][1]}，当前金钱{date_money[idx][1]}，回撤{cur_recall[idx]}")
+    return max_recall
+
+def cal_longest_recall(date_money: list[(str, float)]) -> list[str, int]:
+    longest_recall = []
+    cur_longest = 0
+    cur_max = []
+    for item in date_money:
+        if len(cur_max) == 0 or item[1] > cur_max[-1][1]:
+            cur_max.append(item)
+        else:
+            cur_max.append(cur_max[-1])
+        if item[1] < cur_max[-1][1]:
+            cur_longest += 1
+        else:
+            if len(longest_recall) == 0 or cur_longest > longest_recall[1]:
+                longest_recall = [item[0], cur_longest]
+            cur_longest = 0
+    return longest_recall
+
+
+def plot_profit(date_money: list[(str, float)]):
+    '''
+    绘制净值曲线
+    :param date_money:
+    :return:
+    '''
+    plt.figure(figsize=(15, 7), dpi=100)
+    plt.ylabel('Profit')
+    plt.xlabel('Date')
+    x_major_locator = MultipleLocator(int(len(date_money) / 15))
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(x_major_locator)
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.plot([row[0] for row in date_money], [row[1]/date_money[0][1] for row in date_money])
+    plt.show()
+
+
 if __name__ == '__main__':
-    data_each_day = execute(
-        {"./511520.xls": 0.3,
-         "./000055.xls": 0.2,
-         "./512890.xls": 0.5,
-         }, "2024.12.14", "2025.4.20", 30
-    )
-    money = []
-    for row in data_each_day:
-        money.append(row[-1])
-    # 计算年化收益率
-    # R = (1 + P) ^ (m / n) - 1
-    # 1 + P = 末期资产/初期资产，m = 365, n = 执行天数
+    start_timestamp = pd.Timestamp('2022.08.09').timestamp()
+    end_timestamp = pd.Timestamp('2025.4.20').timestamp()
+    shortest_days = 180 # 最短持有
+    while start_timestamp < end_timestamp - shortest_days * 24 * 60 * 60:
+        start_test_date = pd.to_datetime(start_timestamp, unit='s').strftime("%Y.%m.%d")
+        end_test_date = pd.to_datetime(end_timestamp, unit='s').strftime("%Y.%m.%d")
+        print(f'开始测试，买入日期：{start_test_date} ===============')
+        data_each_day = execute(
+            {
+                # "./511520.xls": 1, # 债
+                "./000055.xls": 1, # 纳指
+                # "./008114.xls": 0.5,  # 红利
+             }, start_test_date, end_test_date, 30
+        )
+        date_money = []
+        for row in data_each_day:
+            date_money.append((row[0], row[-1]))
 
-    # 计算波动率
+        # 计算年化收益率
+        # R = (1 + P) ^ (m / n) - 1
+        # 1 + P = 末期资产/初期资产，m = 365, n = 执行天数
+        yearly_rate = cal_yearly_rate(date_money)
+        print(f'年化{yearly_rate*100}%')
 
-    # 计算最大回撤
+        # 计算波动率
 
-    # 计算最长回撤
+        # 计算最大回撤
+        max_recall = cal_max_recall(date_money)
 
-    # 计算夏普比率？（无风险利率如何获取）
+        # 计算最长回撤
+        longest_recall = cal_longest_recall(date_money)
+        print(f'最长回撤终止日{longest_recall[0]}, 持续时间{longest_recall[1]}')
+
+        # 计算夏普比率？（无风险利率如何获取）
+
+        # 绘制净值曲线
+        # plot_profit(date_money)
+
+        print("end")
+        start_timestamp += 24 * 60 * 60 * 30
